c8d3b93d993584bce3688d5377f71b9de78e4738
Ugorcak, Karol Sokuro <karol.ugorcak@adesso.ch>
2021-05-31 14:16:59 +0200

    init commit

---
+++ src/main/java/com/sokuro/imagegallery/ImagegalleryApplication.java
@@ -0,0 +1,13 @@
+package com.sokuro.imagegallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagegalleryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImagegalleryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sokuro/imagegallery/ImagegalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sokuro.imagegallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagegalleryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
